ea321308ef877216ad5d488c74d618e5d9b8373d
wangxuejiao_00 <wangxuejiao_00@163.com>
2017-10-19 19:21:31 +0800

    wxj:完成日历功能

--- app/src/main/java/com/rgcloud/activity/CalendarActivity.java
+++ app/src/main/java/com/rgcloud/activity/CalendarActivity.java
@@ -5,11 +5,11 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.listener.OnItemClickListener;
-import com.loonggg.weekcalendar.view.WeekCalendar;
+import com.necer.ncalendar.calendar.WeekCalendar;
+import com.necer.ncalendar.listener.OnWeekCalendarChangedListener;
 import com.rgcloud.R;
 import com.rgcloud.adapter.ActivityAdapter;
 import com.rgcloud.config.Constant;
@@ -22,32 +22,32 @@ import com.rgcloud.http.ResponseCallBack;
 import com.rgcloud.util.CirCleLoadingDialogUtil;
 import com.rgcloud.util.ToastUtil;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.joda.time.DateTime;
+
+import java.util.Calendar;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import in.srain.cube.views.ptr.PtrClassicFrameLayout;
+import in.srain.cube.views.ptr.PtrDefaultHandler;
+import in.srain.cube.views.ptr.PtrFrameLayout;
 
 
 public class CalendarActivity extends BaseActivity {
 
-/*    @Bind(R.id.ptr_classic_frame_layout)
-    PtrClassicFrameLayout ptrClassicFrameLayout;*/
+    @Bind(R.id.ptr_classic_frame_layout)
+    PtrClassicFrameLayout ptrClassicFrameLayout;
     @Bind(R.id.tv_calendar)
     TextView tvCalendar;
-   /* @Bind(R.id.rv_calendar)
-    RecyclerView rvCalendar;*/
     @Bind(R.id.week_calendar)
     WeekCalendar weekCalendar;
+
     @Bind(R.id.rv_calendar_activity)
     RecyclerView rvCalendarActivity;
 
     private ActivityAdapter mActivityAdapter;
-  //  private CalendarAdapter mCalendarAdapter;
-    List<ActivityResEntity.WeekDayBean> mWeekDayBeanList = new ArrayList<>();
     private ActivityResEntity mActivityResEntity;
-
     private Integer mSelectedDay;
 
     private boolean mIsEnd;
@@ -65,49 +65,34 @@ public class CalendarActivity extends BaseActivity {
 
     private void initView() {
 
-        List<String> list = new ArrayList<>();
-        list.add("2017-09-13");
-        list.add("2017-10-13");
-        list.add("2017-10-11");
-        list.add("2017-10-10");
-        list.add("2017-10-16");
-//传入已经预约或者曾经要展示选中的时间列表
-        weekCalendar.setSelectDates(list);
 
-        //设置日历点击事件
-        weekCalendar.setOnDateClickListener(new WeekCalendar.OnDateClickListener() {
+        Calendar c = Calendar.getInstance();  //获取Calendar的方法
+        int month = c.get(Calendar.MONTH) + 1;//获取当前月份
+        int day = c.get(Calendar.DAY_OF_MONTH);//获取当前月份的日期号码
+        String monthStr;
+        String dayStr;
+        if (month < 10) {
+            monthStr = "0" + month;
+        } else {
+            monthStr = "" + month;
+        }
+        if (day < 10) {
+            dayStr = "0" + day;
+        } else {
+            dayStr = "" + day;
+        }
+
+        weekCalendar.setDate(c.get(Calendar.YEAR) + "-" + monthStr + "-" + dayStr);
+        weekCalendar.setDefaultSelect(false);
+        weekCalendar.setOnWeekCalendarChangedListener(new OnWeekCalendarChangedListener() {
             @Override
-            public void onDateClick(String time) {
-                Toast.makeText(mContext, time, Toast.LENGTH_SHORT).show();
-            }
-        });
-//设置年月时间的回调
-        weekCalendar.setOnCurrentMonthDateListener(new WeekCalendar.OnCurrentMonthDateListener() {
-            @Override
-            public void onCallbackMonthDate(String year, String month) {
-                Toast.makeText(mContext, year + "-" + month, Toast.LENGTH_SHORT).show();
-            }
-        });
-
-
-
-
-        //  rvCalendar.setLayoutManager(new GridLayoutManager(mContext, 7));
-      //  mCalendarAdapter = new CalendarAdapter(mWeekDayBeanList);
-      //  rvCalendar.setAdapter(mCalendarAdapter);
-
-      /*  rvCalendar.addOnItemTouchListener(new OnItemClickListener() {
-            @Override
-            public void onSimpleItemClick(BaseQuickAdapter adapter, View view, int position) {
-                for (int i = 0; i < mWeekDayBeanList.size(); i++) {
-                    ActivityResEntity.WeekDayBean weekDayBean = mWeekDayBeanList.get(i);
-                    weekDayBean.hasSelected = i == position;
-                }
-                mCalendarAdapter.notifyDataSetChanged();
-                mSelectedDay = mWeekDayBeanList.get(position).PostDay;
+            public void onWeekCalendarChanged(DateTime dateTime) {
+                mSelectedDay = Integer.valueOf("" + dateTime.getYear() + dateTime.getMonthOfYear() + dateTime.getDayOfMonth());
+                tvCalendar.setText(dateTime.getYear() + "年" + dateTime.getMonthOfYear() + "月");
                 getActivities();
             }
-        });*/
+        });
+
 
         rvCalendarActivity.setLayoutManager(new LinearLayoutManager(mContext));
         rvCalendarActivity.addItemDecoration(new HorizontalDividerItemDecoration.Builder(mContext).spaceResId(R.dimen.x10).showLastDivider().build());
@@ -123,13 +108,13 @@ public class CalendarActivity extends BaseActivity {
             }
         });
 
-/*        ptrClassicFrameLayout.setPtrHandler(new PtrDefaultHandler() {
+        ptrClassicFrameLayout.setPtrHandler(new PtrDefaultHandler() {
             @Override
             public void onRefreshBegin(PtrFrameLayout frame) {
                 mPageIndex = 1;
                 getActivities();
             }
-        });*/
+        });
 
         mActivityAdapter.setOnLoadMoreListener(new BaseQuickAdapter.RequestLoadMoreListener() {
             @Override
@@ -166,20 +151,10 @@ public class CalendarActivity extends BaseActivity {
                 super.onObjectResponse(resEntity);
                 if (resEntity == null) return;
                 mActivityResEntity = (ActivityResEntity) resEntity;
-            /*    if (mWeekDayBeanList.size() == 0) {
-                    mWeekDayBeanList.addAll(mActivityResEntity.WeekDay);
-                    mWeekDayBeanList.get(3).hasSelected = true;
-                    mCalendarAdapter.setNewData(mWeekDayBeanList);
-
-                    String postDay = String.valueOf(mWeekDayBeanList.get(3).PostDay);
-                    String year = postDay.substring(0, 4);
-                    String month = postDay.substring(4, 6);
-                    tvCalendar.setText(year + "." + month);
-                }*/
 
                 if (mPageIndex == 1) {
                     mActivityAdapter.setNewData(mActivityResEntity.ActiveList);
-                    //ptrClassicFrameLayout.refreshComplete();
+                    ptrClassicFrameLayout.refreshComplete();
                     mActivityAdapter.disableLoadMoreIfNotFullPage(rvCalendarActivity);
                 } else {
                     mActivityAdapter.addData(mActivityResEntity.ActiveList);
@@ -196,6 +171,7 @@ public class CalendarActivity extends BaseActivity {
         });
     }
 
+
     @OnClick({R.id.iv_back})
     public void onViewClicked(View view) {
         switch (view.getId()) {
@@ -204,4 +180,6 @@ public class CalendarActivity extends BaseActivity {
                 break;
         }
     }
+
+
 }
--- app/src/main/java/com/tencent/qcloud/xiaozhibo/TCApplication.java
+++ app/src/main/java/com/tencent/qcloud/xiaozhibo/TCApplication.java
@@ -41,7 +41,7 @@ public class TCApplication extends MultiDexApplication {
 
         instance = this;
 
-       //initSDK();
+       initSDK();
 
         //配置分享第三方平台的appkey
         PlatformConfig.setWeixin(TCConstants.WEIXIN_SHARE_ID, TCConstants.WEIXIN_SHARE_SECRECT);
@@ -56,7 +56,7 @@ public class TCApplication extends MultiDexApplication {
         JPushInterface.init(this);
 
         // 在使用 SDK 各组间之前初始化 context 信息，传入 ApplicationContext
-     //   SDKInitializer.initialize(this);
+        SDKInitializer.initialize(this);
         //自4.3.0起，百度地图SDK所有接口均支持百度坐标和国测局坐标，用此方法设置您使用的坐标类型.
         //包括BD09LL和GCJ02两种坐标，默认是BD09LL坐标。
         SDKInitializer.setCoordType(CoordType.BD09LL);
